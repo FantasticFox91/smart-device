94cb9d3f56c63dafc73a363d74f8195376b16fae
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-10-19 17:52:11 +0300

    working on polishing project

--- source/js/main.js
+++ source/js/main.js
@@ -5,12 +5,8 @@ import {initModals} from './modules/modals/init-modals';
 
 window.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('.accordion')) {
-    document.querySelectorAll('.accordion__heading').forEach((el) => el.classList.toggle('accordion__heading--close'));
-    document.querySelector('.footer').addEventListener('click', (e) => {
-      if (e.target.classList.contains('accordion__heading')) {
-        e.target.classList.toggle('accordion__heading--close');
-      }
-    });
+    hideAccrodions();
+    document.querySelector('.footer').addEventListener('click', (e) => showAccordion(e));
   }
 
   if (document.querySelector('[data-show-more]')) {
@@ -26,6 +22,58 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  const input = document.querySelector('input[type="tel"]');
+
+  const prefixNumber = (str) => {
+    if (str === '7') {
+      return '7 (';
+    }
+    if (str === '8') {
+      return '8 (';
+    }
+    if (str === '9') {
+      return '7 (9';
+    }
+    return '7 (';
+  };
+
+  // ======================================
+  input.addEventListener('input', (e) => {
+    const value = input.value.replace(/\D+/g, '');
+    const numberLength = 11;
+
+    let result;
+    if (input.value.includes('+8') || input.value[0] === '8') {
+      result = '';
+    } else {
+      result = '+';
+    }
+
+    //
+    for (let i = 0; i < value.length && i < numberLength; i++) {
+      switch (i) {
+        case 0:
+          result += prefixNumber(value[i]);
+          continue;
+        case 4:
+          result += ') ';
+          break;
+        case 7:
+          result += '-';
+          break;
+        case 9:
+          result += '-';
+          break;
+        default:
+          break;
+      }
+      result += value[i];
+    }
+    //
+    input.value = result;
+  });
+
+
   // Utils
   // ---------------------------------
 
@@ -40,6 +88,23 @@ window.addEventListener('DOMContentLoaded', () => {
     initModals();
   });
 });
+
+const showAccordion = (e) => {
+  const accordions = document.querySelectorAll('.accordion__heading');
+  if (e.target.classList.contains('accordion__heading')) {
+    e.target.classList.toggle('accordion__heading--close');
+    const opendAccordions = document.querySelectorAll('.accordion__heading--close');
+    if (opendAccordions.length === 0) {
+      accordions.forEach((el) => el.classList.add('accordion__heading--close'));
+      e.target.classList.toggle('accordion__heading--close');
+    }
+  }
+};
+
+const hideAccrodions = () => {
+  const accordions = document.querySelectorAll('.accordion__heading');
+  accordions.forEach((el) => el.classList.toggle('accordion__heading--close'));
+};
 
 // ---------------------------------
 
