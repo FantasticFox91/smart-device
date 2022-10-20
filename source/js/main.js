e6e0aaf89cba95bce09dc9c39c0e8317f8f83451
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-10-20 12:53:17 +0300

    working on polishing project

--- source/js/main.js
+++ source/js/main.js
@@ -22,57 +22,55 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  const input = document.querySelector('input[type="tel"]');
+  if (document.querySelector('input[type="tel"]')) {
+    const inputs = document.querySelectorAll('input[type="tel"]');
 
-  const prefixNumber = (str) => {
-    if (str === '7') {
-      return '7 (';
-    }
-    if (str === '8') {
-      return '8 (';
-    }
-    if (str === '9') {
-      return '7 (9';
-    }
-    return '7 (';
-  };
-
-  // ======================================
-  input.addEventListener('input', (e) => {
-    const value = input.value.replace(/\D+/g, '');
-    const numberLength = 11;
-
-    let result;
-    if (input.value.includes('+8') || input.value[0] === '8') {
-      result = '';
-    } else {
-      result = '+';
-    }
-
-    //
-    for (let i = 0; i < value.length && i < numberLength; i++) {
-      switch (i) {
-        case 0:
-          result += prefixNumber(value[i]);
-          continue;
-        case 4:
-          result += ') ';
-          break;
-        case 7:
-          result += '-';
-          break;
-        case 9:
-          result += '-';
-          break;
-        default:
-          break;
+    const prefixNumber = (str) => {
+      if (str === '7') {
+        return '7 (';
       }
-      result += value[i];
-    }
-    //
-    input.value = result;
-  });
+      if (str === '8') {
+        return '8 (';
+      }
+      if (str === '9') {
+        return '7 (9';
+      }
+      return '7 (';
+    };
 
+    inputs.forEach((input) => input.addEventListener('input', () => {
+      const value = input.value.replace(/\D+/g, '');
+      const numberLength = 11;
+
+      let result;
+      if (input.value.includes('+8') || input.value[0] === '8') {
+        result = '';
+      } else {
+        result = '+';
+      }
+
+      for (let i = 0; i < value.length && i < numberLength; i++) {
+        switch (i) {
+          case 0:
+            result += prefixNumber(value[i]);
+            continue;
+          case 4:
+            result += ') ';
+            break;
+          case 7:
+            result += '-';
+            break;
+          case 9:
+            result += '-';
+            break;
+          default:
+            break;
+        }
+        result += value[i];
+      }
+      input.value = result;
+    }));
+  }
 
   // Utils
   // ---------------------------------
